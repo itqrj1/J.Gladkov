2d3eaf81caee3bd2a41ac6111a578c7efd66b5f6
itqrj1 <40244004+itqrj1@users.noreply.github.com>
2020-02-15 11:47:23 +0300

    Add files via upload

---
+++ Sc/scool_15.02.py
@@ -0,0 +1,47 @@
+#import numpy as np
+
+#c =4
+#matrix = np.numpy[[1,2,3],[4,5,6,],[7,8,9]]
+
+#print(matrix)
+
+#transpose_matrix = matrix.transpose()
+
+#print(transpose_matrix)
+
+
+#print(np.delet(transpose_matrix, 0,1))
+#print(____________)
+
+
+#def max (arr = []):
+
+ #  max = arr.sort()
+ #   max = arr[0]
+#    for i in arr[1::1]:
+#        if i >= max:
+#            max =1
+#    return max
+#    return arr [-1]
+#pass
+#array = [123,1233,455465,27456,8345345,1,23,13,45,53453,45]
+#max_item = max(array)
+#print(max_item)
+#def myenurate(items = []):
+ #   res = []
+ #   j = 0
+ #   for i in items:
+ #       res.append(tuple(j, i))
+#    for i in items :
+ #       print(i)
+
+ #   return res
+#
+
+#a = [1,2,3,4]
+
+#for i in myenurate(a):
+#    print(i)
+
+
+
